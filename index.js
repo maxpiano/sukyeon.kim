74b0da17f06d085de4839339622b8e3427248707
Max B <maxbilov@gmail.com>
2020-04-18 13:37:34 +0200

    css changes

---
+++ index.js
@@ -0,0 +1,24 @@
+const buttons = document.querySelectorAll(".navigation li");
+const mobileButtons = document.querySelectorAll(".mobile-navigation li");
+const slides = document.querySelectorAll(".content div");
+const header = document.querySelector("header");
+
+buttons.forEach((button) =>
+  button.addEventListener("click", function () {
+    window.scrollTo({
+      top: slides[this.dataset.index].offsetTop - header.offsetHeight,
+      left: slides[this.dataset.index].offsetLeft,
+      behavior: "smooth",
+    });
+  })
+);
+
+mobileButtons.forEach((button) =>
+  button.addEventListener("click", function () {
+    window.scrollTo({
+      top: slides[this.dataset.index].offsetTop - header.offsetHeight,
+      left: slides[this.dataset.index].offsetLeft,
+      behavior: "smooth",
+    });
+  })
+);
